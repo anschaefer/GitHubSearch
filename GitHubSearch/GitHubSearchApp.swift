004c2a8c107c0d54b60b0706abeca5af51ee3854
Andre Schaefer <andre.schaefer@codecentric.de>
2024-02-21 07:31:09 +0100

    Initial Commit

---
+++ GitHubSearch/GitHubSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSearchApp.swift
+//  GitHubSearch
+//
+//  Created by André Schäfer on 21.02.24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
